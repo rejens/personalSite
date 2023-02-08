614b75171f67ce73231659a9ddec81ce163e7597
Rejens <rejensraya@gmail.com>
2023-02-08 11:37:35 +0545

    kam vyoo

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import Myself from "../assets/image/myself.png";
+import { FaBeer } from "react-icons/fa";
+import { FaFacebookSquare } from "react-icons/fa";
+import Zoro from "../assets/image/zoro.jpg";
 
 function Main() {
   return (
     <div className="main-background">
       <div className="card">
-        <img
-          className="myImg"
-          src="https://i.pinimg.com/736x/44/d4/64/44d464090c6de8811959fa793bd73d98.jpg"
-          alt=""
-        />
+        <img className="myImg" src={Zoro} alt="" />
         <div className="text">
           <div className="name">Rejens</div>
           <div className="description">
             The darker the night, the brighter the stars
+          </div>
+          <div className="links">
+            <a
+              href="https://www.facebook.com/rejens.rayamajhi.73/"
+              target="_blank"
+            >
+              {" "}
+              <FaFacebookSquare />
+            </a>
           </div>
         </div>
       </div>
