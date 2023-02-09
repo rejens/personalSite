53920154a11e9a176987c6830b4d56708e0077fb
Rejens <rejensraya@gmail.com>
2023-02-09 21:06:22 +0545

    quote api added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Myself from "../assets/image/myself.png";
 import { FaBeer } from "react-icons/fa";
 import { FaFacebookSquare } from "react-icons/fa";
 import Zoro from "../assets/image/zoro.jpg";
 
 function Main() {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  useEffect(() => {
+    (async function fetchData() {
+      let value = await fetch("https://api.quotable.io/random");
+      value = await value.json();
+      setQuote(value.content);
+      setAuthor("~ " + value.author);
+    })();
+  }, []);
+
   return (
     <div className="main-background">
       <div className="card">
         <img className="myImg" src={Zoro} alt="" />
         <div className="text">
           <div className="name">Rejens</div>
-          <div className="description">
-            The darker the night, the brighter the stars
-          </div>
+          <div className="description">{quote}</div>
+          <div className="author">{author}</div>
           <div className="links">
             <a
               href="https://www.facebook.com/rejens.rayamajhi.73/"
