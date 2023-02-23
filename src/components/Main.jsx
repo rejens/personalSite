664e7d545b566f82562e11721598de224c0e32b9
Rejens <rejensraya@gmail.com>
2023-02-23 20:57:53 +0545

    spinner

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,27 +1,31 @@
 import React, { useEffect, useState } from "react";
 import { FaFacebookSquare } from "react-icons/fa";
 import Zoro from "../assets/image/zoro.jpg";
+import Loading from "../assets/image/spinner.gif";
 
 function Main() {
   const [quote, setQuote] = useState("");
   const [author, setAuthor] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     (async function fetchData() {
       let value = await fetch("https://api.quotable.io/random");
       value = await value.json();
-
       setQuote(value.content);
       setAuthor("~ " + value.author);
+      setIsLoading(false);
     })();
   }, []);
 
+  let spinner = isLoading ? <img width="50px" src={Loading} alt="" /> : "";
   return (
     <div className="main-background">
       <div className="card">
         <img className="myImg" src={Zoro} alt="" />
         <div className="text">
           <div className="name">Rejens</div>
+          {spinner}
           <div className="description">{quote}</div>
           <div className="author">{author}</div>
           <div className="links">
