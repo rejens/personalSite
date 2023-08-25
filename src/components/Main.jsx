c49bb52c4ebd05a7313bee19cb861ec547ee7ad7
Rejens <rejensraya@gmail.com>
2023-08-25 22:59:05 +0545

    timing lai feri 7.5 sec banako

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,53 +4,55 @@ import Zoro from "../assets/image/zoro.jpg";
 import Loading from "../assets/image/spinner.gif";
 
 function Main() {
-  const [quote, setQuote] = useState("");
-  const [author, setAuthor] = useState("");
-  const [isLoading, setIsLoading] = useState(true);
-  const [time, setTime] = useState();
+   const [quote, setQuote] = useState("");
+   const [author, setAuthor] = useState("");
+   const [isLoading, setIsLoading] = useState(true);
+   const [time, setTime] = useState();
 
-  const fetchData = async () => {
-    let value = await fetch("https://api.quotable.io/random");
-    value = await value.json();
-    setQuote(value.content);
-    setAuthor("~ " + value.author);
-    setIsLoading(false);
-  };
+   const fetchData = async () => {
+      let value = await fetch("https://api.quotable.io/random");
+      value = await value.json();
+      setQuote(value.content);
+      setAuthor("~ " + value.author);
+      setIsLoading(false);
+   };
 
-  useEffect(() => {
-    fetchData();
-    const interval = setInterval(() => {
+   useEffect(() => {
       fetchData();
-      // console.log("display");
-    }, 5000);
-    return () => clearInterval(interval);
-  }, []);
+      const interval = setInterval(() => {
+         fetchData();
+         // console.log("display");
+      }, 7500);
+      return () => clearInterval(interval);
+   }, []);
 
-  // let spinner = isLoading ? <img width="50px" src={Loading} alt="" /> : "";
-  return (
-    <div className="main-background">
-      <div className="card">
-        <img className="myImg" src={Zoro} alt="" />
-        <div className="text">
-          <div className="name">Rejens</div>
-          {isLoading && <img width="50px" src={Loading} alt="" />}
-          {/* {spinner} */}
+   // let spinner = isLoading ? <img width="50px" src={Loading} alt="" /> : "";
+   return (
+      <div className="main-background">
+         <div className="card">
+            <div>
+               <img className="myImg" src={Zoro} alt="" />
+               <div className="text">
+                  <div className="name">Rejens</div>
+                  {isLoading && <img width="50px" src={Loading} alt="" />}
+                  {/* {spinner} */}
 
-          <div className="description">{quote}</div>
-          <div className="author">{author}</div>
-          <div className="links">
-            <a
-              href="https://www.facebook.com/rejens.rayamajhi.73/"
-              target="_blank"
-            >
-              {" "}
-              <FaFacebookSquare />
-            </a>
-          </div>
-        </div>
+                  <div className="description">{quote}</div>
+                  <div className="author">{author}</div>
+               </div>
+            </div>
+            <div className="links">
+               <a
+                  href="https://www.facebook.com/rejens.rayamajhi.73/"
+                  target="_blank"
+               >
+                  {" "}
+                  <FaFacebookSquare />
+               </a>
+            </div>
+         </div>
       </div>
-    </div>
-  );
+   );
 }
 
 export default Main;
